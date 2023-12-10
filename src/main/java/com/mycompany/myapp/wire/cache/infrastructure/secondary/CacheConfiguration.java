790cd41dbfefa2d973c6c801a2cb3e8ed12695a1
Gaëtan Bloch <gaetan.bloch@keyconsulting.fr>
2023-12-10 07:44:17 +0100

    Apply spring-boot-cache module

---
+++ src/main/java/com/mycompany/myapp/wire/cache/infrastructure/secondary/CacheConfiguration.java
@@ -0,0 +1,8 @@
+package com.mycompany.myapp.wire.cache.infrastructure.secondary;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+class CacheConfiguration {}
